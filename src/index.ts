270930e8d7c4f47032d7ad3ffed8dde377d5ddca
Tomer Shomron <tomercpc01@gmail.com>
2025-01-17 21:41:12 +0200

    start system

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 // tomer-shomron-324205145-omer-hasid-322994120
+
+import { environmentVariables } from './config';
+import { createSystemConfig } from './services/system/config';
+import { System } from './services/system/system';
+
+const systemConfig = createSystemConfig(environmentVariables);
+const system = new System(systemConfig);
+
+system.start().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
