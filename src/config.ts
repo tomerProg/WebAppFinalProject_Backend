51b031a0924e56b6407cc5c35a64f5baf0c0e209
Tomer Shomron <tomercpc01@gmail.com>
2025-01-17 21:40:10 +0200

    schema for env

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+dotenv.config();
+
+const environmentVariablesZodScema = z.object({
+    PORT: z.coerce.number()
+});
+export type EnvironmentVariables = z.infer<typeof environmentVariablesZodScema>;
+
+const validation = environmentVariablesZodScema.safeParse(process.env);
+if (validation.error) {
+    console.error('Invalid Env: ', validation.error);
+    process.exit(1);
+}
+
+export const environmentVariables = validation.data;
