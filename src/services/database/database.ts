9c1a767d6902164d9fde484d23914220cd2b2711
Tomer Shomron <tomercpc01@gmail.com>
2025-01-18 05:06:56 +0200

    added database dependency to system

--- src/services/database/database.ts
+++ src/services/database/database.ts
@@ -1,7 +1,12 @@
 import { userModel } from '../../users/model';
 import { Service } from '../service';
+import { DatabaseConfig } from './config';
 
 export class Database extends Service {
+    constructor(private readonly config: DatabaseConfig) {
+        super();
+    }
+    
     getModels = () => {
         userModel;
     };
--- src/services/system/system.ts
+++ src/services/system/system.ts
@@ -1,14 +1,16 @@
+import { Database } from '../database/database';
 import { Server } from '../server/server';
 import { Service } from '../service';
 import { SystemConfig } from './config';
 
 export class System extends Service {
     private readonly server: Server;
-
+    private readonly database: Database;
     constructor(private readonly config: SystemConfig) {
         super();
-        const { serverConfig } = this.config;
-        this.server = new Server(serverConfig);
+        const { serverConfig, databaseConfig } = this.config;
+        this.database = new Database(databaseConfig);
+        this.server = new Server(serverConfig, { database: this.database });
     }
 
     async start() {
