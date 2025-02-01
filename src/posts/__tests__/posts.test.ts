1dc5d92b0f32f80b4bb557f6b170b3fcb710cfc3
omer hasid <omerhasid11@gmail.com>
2025-02-01 17:14:13 +0200

    test generate suggestion

--- src/posts/__tests__/posts.test.ts
+++ src/posts/__tests__/posts.test.ts
@@ -31,7 +31,8 @@ describe('posts route', () => {
         _id: new Types.ObjectId(),
         title: 'Post Title', 
         owner: loginUser._id.toString(), 
-        description: 'post description'
+        description: 'post description',
+        suggestion: 'suggestion'
     };
 
     const postsRouter = createPostsRouter(authMiddleware, { postModel });
@@ -262,9 +263,7 @@ describe('posts route', () => {
                 ...testPost,
                 owner: otherUserId
             })
-    
-            const createdPost = await postModel.findById(testPost._id).lean();
-    
+        
             expect(response.status).toBe(StatusCodes.OK);
             expect(response.body).not.toBeNull();
             expect(response.body?.owner).toStrictEqual(loginUser._id.toString());
@@ -273,6 +272,28 @@ describe('posts route', () => {
             expect(response.body?.suggestion).toStrictEqual(testPost.suggestion);
             expect(response.body?.imageSrc).toStrictEqual(testPost.imageSrc);
         });
+
+        test('suggestion is generated if the user does not give one', async () => {
+            const postWithoutSuggestion: Post = {
+                title: 'Replacing light bulb',
+                owner: loginUser._id.toString(),  
+                description: 'Replacing old light bulb with a new one'
+            }
+            const response = await request(app).post('/post').send({
+                ...postWithoutSuggestion,
+                owner: loginUser._id.toString(),
+            })
+        
+            expect(response.status).toBe(StatusCodes.OK);
+            expect(response.body).not.toBeNull();
+            expect(response.body?.suggestion).not.toBeNull();
+
+            expect(response.body?.owner).toStrictEqual(loginUser._id.toString());
+            expect(response.body?.title).toStrictEqual(postWithoutSuggestion.title);
+            expect(response.body?.description).toStrictEqual(postWithoutSuggestion.description);
+            expect(response.body?.imageSrc).toStrictEqual(postWithoutSuggestion.imageSrc);
+        });
+
     
         test('user cannot create post without required fields', async () => {
             await postModel.deleteOne({ _id: testPost._id });
