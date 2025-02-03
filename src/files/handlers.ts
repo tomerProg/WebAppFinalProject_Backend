b6e94e6c06785ae7be0aa2bcaacb0232abecbb70
Tomer Shomron <tomercpc01@gmail.com>
2025-02-03 23:38:42 +0200

    fix send type

--- src/files/handlers.ts
+++ src/files/handlers.ts
@@ -1,14 +1,13 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { InternalServerError } from '../services/server/exceptions';
 
 export const responseForUploadedFile =
     (serverUrl: string, prefix: string = '') =>
-    (req: Request, res: Response) => {
+    (req: Request, res: Response, next: NextFunction) => {
         if (!req.file) {
-            res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+            next(new InternalServerError('file is not exist in request'));
         } else {
-            res.status(StatusCodes.OK).send({
-                url: serverUrl + prefix + req.file.path
-            });
+            res.status(StatusCodes.OK).send(serverUrl + prefix + req.file.filename);
         }
     };
