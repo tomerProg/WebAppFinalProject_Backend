b8f2b9b8697148934876ef10ef122d529c3e0a83
Tomer Shomron <tomercpc01@gmail.com>
2025-03-02 00:17:41 +0200

    fix prod build

--- src/authentication/config.ts
+++ src/authentication/config.ts
@@ -1,15 +1,16 @@
 import { EnvironmentVariables } from '../config';
+import {SignOptions} from 'jsonwebtoken'
 
 export const REFRESH_TOKEN_COOKIE_NAME = 'refresh-token';
 
 export type AuthConfig = {
     tokenSecret: string;
-    tokenExpires: string;
-    refreshTokenExpires: string;
+    tokenExpires: SignOptions['expiresIn'];
+    refreshTokenExpires: SignOptions['expiresIn'];
 };
 
 export const createAuthConfig = (env: EnvironmentVariables): AuthConfig => ({
     tokenSecret: env.AUTH_TOKEN_SECRET,
-    tokenExpires: env.AUTH_TOKEN_EXPIRES,
-    refreshTokenExpires: env.AUTH_REFRESH_TOKEN_EXPIRES
+    tokenExpires: env.AUTH_TOKEN_EXPIRES as SignOptions['expiresIn'],
+    refreshTokenExpires: env.AUTH_REFRESH_TOKEN_EXPIRES  as SignOptions['expiresIn']
 });
