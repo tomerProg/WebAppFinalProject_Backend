90b9b868c453b2c6edb42d3c12435f34e773c362
Tomer Shomron <tomercpc01@gmail.com>
2025-03-01 22:28:01 +0200

    catch system error

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@ import { createSystemConfig } from './services/system/config';
 import { System } from './services/system/system';
 
 const systemConfig = createSystemConfig(environmentVariables);
-const system = new System(systemConfig);
-
-system.start().catch((error) => {
-    console.error(error);
-    process.exit(1);
-});
+try {
+    const system = new System(systemConfig);
+    system.start().catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+} catch (error) {
+    console.error('failed running system', error);
+}
